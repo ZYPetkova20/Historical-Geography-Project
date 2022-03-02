05dac78cfa6a79b704d8b94479061b2f1d296456
Zhivko Spasov <85338649+ZZSpasov20@users.noreply.github.com>
2022-03-02 20:31:19 +0200

    Fix problem with the colors in the main page and make the mini nav in ameriva page

--- Website/src/js/dark-mode.js
+++ Website/src/js/dark-mode.js
@@ -1,5 +1,7 @@
 let dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
+let counter  =0;
+
 var element = document.body;
 var button = document.getElementById('dark-mode-button');
 
@@ -9,33 +11,15 @@ let indexMainBackground = document.getElementById('index-main');
 let scrollDownIcon = document.getElementById('scroll-down');
 let footerBackground = document.querySelector('footer');
 
-
-//by default to be dark or white mode
-if(dark){
-    element.classList.add("dark-mode");
-    element.classList.remove("light-mode");
-    logoImage.src = "dist/images/newtLogoInWhite.png";
-    indexMainBackground.style.backgroundImage = "url(dist/images/sectionBackgroundWhite.png)";
-    footerBackground.style.backgroundImage = "url(dist/images/footerBackgroundDarkMode.png)";
-    button.checked = true;
-}
-else{
-    element.classList.add("light-mode");
-    element.classList.remove("dark-mode");
-    logoImage.src = "dist/images/newtLogo.png";
-    indexMainBackground.style.backgroundImage = "url(dist/images/sectionBackground.png)";
-    footerBackground.style.backgroundImage = "url(dist/images/footerBackground.png)";
-    button.checked = false;
-}
-
-//to change the white or te dark mode
-function darkMode(){
-    if(button.checked == true){
+if(document.body.classList.contains('main-page')){
+    //change images in the main page
+    if(dark){
         element.classList.add("dark-mode");
         element.classList.remove("light-mode");
         logoImage.src = "dist/images/newtLogoInWhite.png";
         indexMainBackground.style.backgroundImage = "url(dist/images/sectionBackgroundWhite.png)";
         footerBackground.style.backgroundImage = "url(dist/images/footerBackgroundDarkMode.png)";
+        button.checked = true;
     }
     else{
         element.classList.add("light-mode");
@@ -43,5 +27,54 @@ function darkMode(){
         logoImage.src = "dist/images/newtLogo.png";
         indexMainBackground.style.backgroundImage = "url(dist/images/sectionBackground.png)";
         footerBackground.style.backgroundImage = "url(dist/images/footerBackground.png)";
+        button.checked = false;
+    }
+    
+    function darkMode(){
+        if(button.checked == true){
+            element.classList.add("dark-mode");
+            element.classList.remove("light-mode");
+            logoImage.src = "dist/images/newtLogoInWhite.png";
+            indexMainBackground.style.backgroundImage = "url(dist/images/sectionBackgroundWhite.png)";
+            footerBackground.style.backgroundImage = "url(dist/images/footerBackgroundDarkMode.png)";
+            
+        }
+        else{
+            element.classList.add("light-mode");
+            element.classList.remove("dark-mode");
+            logoImage.src = "dist/images/newtLogo.png";
+            indexMainBackground.style.backgroundImage = "url(dist/images/sectionBackground.png)";
+            footerBackground.style.backgroundImage = "url(dist/images/footerBackground.png)";
+        }
+    }
+    
+}
+else if(document.body.classList.contains('secondary-page')){
+    //change images in the main page
+    if(dark){
+        element.classList.add("dark-mode");
+        element.classList.remove("light-mode");
+        footerBackground.style.backgroundImage = "url(dist/images/footerBackgroundDarkMode.png)";
+        button.checked = true;
+    }
+    else{
+        element.classList.add("light-mode");
+        element.classList.remove("dark-mode");
+        footerBackground.style.backgroundImage = "url(dist/images/footerBackground.png)";
+        button.checked = false;
+    }
+    
+    //to change the white or te dark mode
+    function darkMode(){
+        if(button.checked == true){
+            element.classList.add("dark-mode");
+            element.classList.remove("light-mode");
+            footerBackground.style.backgroundImage = "url(dist/images/footerBackgroundDarkMode.png)";
+        }
+        else{
+            element.classList.add("light-mode");
+            element.classList.remove("dark-mode");
+            footerBackground.style.backgroundImage = "url(dist/images/footerBackground.png)";
+        }
     }
 }
