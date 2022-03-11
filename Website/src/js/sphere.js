776ad3e05de4319cae16a86f36c646df8bd1ca8b
Zhivko Spasov <85338649+ZZSpasov20@users.noreply.github.com>
2022-03-11 17:57:49 +0200

    Add the planet in the pop up
    Co-Authored-By: VVYaneva20 <85335553+VVYaneva20@users.noreply.github.com>

--- Website/src/js/sphere.js
+++ Website/src/js/sphere.js
@@ -1,4 +1,5 @@
 let popUpDiv = document.getElementById("sphere-pop-up-background");
+let popUpcontent = document.getElementById("sphere-wrapper");
 
 function popUp(){
     popUpDiv.style.display = "flex";
@@ -8,7 +9,7 @@ function closePopUp(){
     popUpDiv.style.display = "none";
     popUpbackground.classList.remove('backgroundAppear');
 }
-/*const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('canvas');
 
 const scene = new THREE.Scene()
         const camera = new THREE.PerspectiveCamera(
@@ -23,26 +24,26 @@ const scene = new THREE.Scene()
             canvas: canvas
         })
 
-        renderer.setSize(800, 400)
-        renderer.setPixelRatio(window.devicePixelRatio)
+        renderer.setSize(1200, 600);
+        renderer.setPixelRatio(window.devicePixelRatio);
         
-        popUpDiv.appendChild(renderer.domElement)
+        popUpcontent.appendChild(renderer.domElement);
 
         //create sphere
         const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), new THREE.MeshBasicMaterial({
             map: new THREE.TextureLoader().load('../images/globe.jpg')
     
-        }))
+        }));
 
-        const group = new THREE.Group()
-        group.add(sphere)
-        scene.add(group)
+        const group = new THREE.Group();
+        group.add(sphere);
+        scene.add(group);
 
         camera.position.z = 15
         const mouse = {
             x: undefined,
             y: undefined
-        }
+        };
         function animate(){
             requestAnimationFrame(animate)
             renderer.render(scene, camera)
@@ -50,4 +51,3 @@ const scene = new THREE.Scene()
          
         }
         animate();
-*/
